70c0500c14fd6813c03b26afcbd85d897b0e93e3
pasha <ap550697@gmail.com>
2024-11-11 01:44:10 -0500

    user_schema

---
+++ backend/services/iam-service/app/domain/schemas/user_schema.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, Field, EmailStr, constr
+
+
+class UserRegisterSchema(BaseModel):
+    password: str
+    user_name: str
+    name: str
+    email: EmailStr
+    phone_number: Optional[constr(max_length=20)]
+    gender: Optional[constr(max_length=10)]
+    date_of_birth: Optional[str]
+    # image: Optional[str]
+
+
+class UserRegisterResponse(BaseModel):
+    id: int
+    user_name: str
+    name: str
+    email: EmailStr
+    phone_number: Optional[str]
+    gender: Optional[str]
+    date_of_birth: Optional[str]
+    image: Optional[str]
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
