8c5f15c88215808164d053528fc285edbf45a8f3
iamErmia <iam.ermia@yahoo.com>
2024-11-21 12:20:34 +0330

    Assests added

---
+++ frontend/fit-plan/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Landing from "./pages/landing";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
---
+++ frontend/fit-plan/src/components/top_navbar.jsx
@@ -0,0 +1,7 @@
+import "../App.css";
+
+export default function Top_navbar() {
+    return(
+        <div></div>
+    );
+}
