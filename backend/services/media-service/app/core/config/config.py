765efe6aae2d2b9356c94101c1eac4e32f85a740
pasha <ap550697@gmail.com>
2024-12-25 11:51:45 -0500

    media first develop

---
+++ backend/services/media-service/app/core/config/config.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+from pathlib import Path
+from loguru import logger
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str =  "mongodb://localhost:27017"   #"mongodb://mongo:27017"
+    DATABASE_NAME: str = "AIToolsBoxMediaDB"
+    FILE_STORAGE_PATH: str = "app/media"
+    IAM_URL: str = "http://iam.localhost"
+    GRPC_PORT: int = "50051"
+
+    # model_config = SettingsConfigDict(env_file=str(Path(__file__).resolve().parent / ".env"))
+
+
+#@lru_cache
+@logger.catch
+def get_settings():
+    return Settings()
