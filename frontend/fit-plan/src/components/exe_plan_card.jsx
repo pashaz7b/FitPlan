e9d67a8da290a95763ca2216bf2bddb956fd1855
iamErmia <iam.ermia@yahoo.com>
2025-01-09 15:54:21 +0330

    Finished the "User Exercise plan" page

--- frontend/fit-plan/src/components/exe_plan_card.jsx
+++ frontend/fit-plan/src/components/exe_plan_card.jsx
@@ -1,44 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Exe_plan_card({
-  name,
-  image,
-  description,
-  onClick,
-  day,
-  movements,
-}) {
+export default function Exe_plan_card({ plan }) {
+  const [completed, setCompleted] = useState(
+    plan.days.map((day) => day.movements.map(() => false))
+  );
+
+  // Handle checkbox toggle
+  const handleToggle = (dayIndex, movementIndex) => {
+    const updatedCompleted = completed.map((day, dIndex) =>
+      dIndex === dayIndex
+        ? day.map((checked, mIndex) =>
+            mIndex === movementIndex ? !checked : checked
+          )
+        : day
+    );
+    setCompleted(updatedCompleted);
+  };
+
   return (
-    <div className="collapse ">
+    // <div className="collapse ">
+    //   <input type="radio" name="my-accordion-1" />
+    //   <div className="collapse-title disabled:hover w-full max-h-[139px] p-0 rounded-[15px] border border-mintCream overflow-hidden flex cursor-pointer transition-all duration-300 hover:bg-superRed">
+    //     <img
+    //       src={image}
+    //       alt={name}
+    //       className="w-[25%] h-full object-cover object-top"
+    //     />
+    //     <div className="w-[75%] h-full flex flex-col gap-2 text-right p-4 transition-all duration-300">
+    //       <p className="text-mintCream text-[35px] font-semibold">{name}</p>
+    //       <p className="text-mintCream text-[20px]">{description}</p>
+    //     </div>
+    //   </div>
+    //   <div className="collapse-content bg-coal rounded-[10px] flex flex-col gap-10 text-mintCream text-center px-4 py-5">
+
+    //   </div>
+    // </div>
+    <div className="collapse max-h-[500px] overflow-y-auto scrollbar-thin scrollbar-thumb-superRed scrollbar-track-coal">
       <input type="radio" name="my-accordion-1" />
       <div className="collapse-title disabled:hover w-full max-h-[139px] p-0 rounded-[15px] border border-mintCream overflow-hidden flex cursor-pointer transition-all duration-300 hover:bg-superRed">
         <img
-          src={image}
-          alt={name}
+          src={plan.image}
+          alt={plan.title}
           className="w-[25%] h-full object-cover object-top"
         />
         <div className="w-[75%] h-full flex flex-col gap-2 text-right p-4 transition-all duration-300">
-          <p className="text-mintCream text-[35px] font-semibold">{name}</p>
-          <p className="text-mintCream text-[20px]">{description}</p>
+          <p className="text-mintCream text-[35px] font-semibold">{plan.title}</p>
+          <p className="text-mintCream text-[20px]">{plan.description}</p>
         </div>
       </div>
       <div className="collapse-content bg-coal rounded-[10px] flex flex-col gap-10 text-mintCream text-center px-4 py-5">
-        <div id="breakfast" className="flex flex-col gap-2 text-right">
-          <h1 className="font-bold text-[30px]">صبحانه</h1>
-          <p className="text-[17px]">{breakfast}</p>
+        <div className="rounded-lg shadow-md p-4 mb-4">
+          {/* <h2 className="text-xl font-bold mb-4">{plan.title}</h2> */}
+          <div className="space-y-4 bg-black">
+            {plan.days.map((day, dayIndex) => (
+              <div
+                key={dayIndex}
+                className="collapse collapse-arrow border border-gray-300 rounded-lg"
+              >
+                <input type="checkbox" />
+                <div className="collapse-title text-[20px] font-semibold">
+                  {day.name}
+                </div>
+                <div className="collapse-content">
+                  <div className="grid max-md:grid-cols-1 grid-cols-2  gap-4 mt-4">
+                    {day.movements.map((movement, movementIndex) => (
+                      <div
+                        key={movementIndex}
+                        className="flex gap-2 items-center bg-coal p-4 rounded-[15px] shadow"
+                      >
+                        <input
+                          type="checkbox"
+                          className="checkbox checkbox-primary mr-3"
+                          checked={completed[dayIndex][movementIndex]}
+                          onChange={() => handleToggle(dayIndex, movementIndex)}
+                        />
+                        <div className="flex justify-between w-full">
+                          <p className="text-lg">{movement.name}</p>
+                          <p className="text-sm text-mintCream">
+                            {movement.repeats}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        <div id="lunch" className="flex flex-col gap-2 text-right">
-          <h1 className="font-bold text-[30px]">ناهار</h1>
-          <p className="text-[17px]">{lunch}</p>
-        </div>
-        <div id="dinner" className="flex flex-col gap-2 text-right">
-          <h1 className="font-bold text-[30px]">شام</h1>
-          <p className="text-[17px]">{dinner}</p>
-        </div>
-        <p className="text-[18px] mt-8">
-          این برنامه تا <span className="text-superRed"> 2ماه دیگر </span>اعتبار
-          دارد
-        </p>
       </div>
     </div>
   );
--- frontend/fit-plan/src/pages/user_exercisePlan.jsx
+++ frontend/fit-plan/src/pages/user_exercisePlan.jsx
@@ -3,6 +3,7 @@ import Plan_card from "../components/plan_card";
 import { Navigate, useNavigate } from "react-router-dom";
 import fit_logo from "/Images/Fit-Logo-Resized.png";
 import { useEffect } from "react";
+import Exe_plan_card from "../components/exe_plan_card";
 
 export default function User_exercisePlan() {
   const [userInfo, setUserInfo] = useState({
@@ -35,17 +36,54 @@ export default function User_exercisePlan() {
   });
 
   const exercisePlans = [
+    // {
+    //   day: "Day 1",
+    //   name: coachInfo.nameSurname,
+    //   image: coachInfo.image,
+    //   description: "شما یک برنامه تمرینی جدید دریافت کردید!",
+    //   movements: ["Push-ups", "Pull-ups"],
+    // },
     {
-      day: "Day 1",
-      movements: ["Push-ups", "Pull-ups"],
+      title: coachInfo.nameSurname,
+      image: coachInfo.image,
+      description: "شما یک برنامه تمرینی جدید دریافت کردید!",
+      days: [
+        {
+          name: "روز اول",
+          movements: [
+            { name: "Push-ups", repeats: "3X10" },
+            { name: "Pull-ups", repeats: "3X12" },
+          ],
+        },
+        {
+          name: "روز دوم",
+          movements: [
+            { name: "Squats", repeats: "12-10-8" },
+            { name: "Lunges", repeats: "3 دقیقه" },
+          ],
+        },
+      ],
     },
     {
-      day: "Day 2",
-      movements: ["Squats", "Lunges"],
-    },
-    {
-      day: "Day 3",
-      movements: ["Bench Press", "Deadlift"],
+      title: coachInfo.nameSurname,
+      image: coachInfo.image,
+      description: "شما یک برنامه تمرینی جدید دریافت کردید!",
+      days: [
+        {
+          name: "روز اول",
+          movements: [
+            { name: "Push-ups", repeats: "3X10" },
+            { name: "Pull-ups", repeats: "3X12" },
+          ],
+        },
+        {
+          name: "روز دوم",
+          movements: [
+            { name: "Squats", repeats: "12-10-8" },
+            { name: "Lunges", repeats: "3 دقیقه" },
+          ],
+        },
+      ],
     },
   ];
 
@@ -269,7 +307,7 @@ export default function User_exercisePlan() {
                 <div className="flex flex-col gap-1 p-2">
                   <a
                     href="/user_panel"
-                    className="w-[90%] border-crimsonRed bg-crimsonRed text-black text-[20px] flex justify-between rounded-[10px] max-h-[58px] mx-auto mt-3 py-3 px-3 hover:bg-superRed hover:border-superRed transition-all duration-300"
+                    className="w-[90%] border-[2px] border-crimsonRed bg-coal text-mintCream text-[20px] flex justify-between rounded-[10px] max-h-[58px] mx-auto mt-3 py-3 px-3 hover:bg-superRed hover:border-superRed transition-all duration-300"
                   >
                     <p>اطلاعات کاربر</p>
                     <svg
@@ -277,7 +315,7 @@ export default function User_exercisePlan() {
                       height="35px"
                       viewBox="0 -960 960 960"
                       width="35px"
-                      fill="#000000"
+                      fill="#e8eaed"
                     >
                       <path d="M480-492.31q-57.75 0-98.87-41.12Q340-574.56 340-632.31q0-57.75 41.13-98.87 41.12-41.13 98.87-41.13 57.75 0 98.87 41.13Q620-690.06 620-632.31q0 57.75-41.13 98.88-41.12 41.12-98.87 41.12ZM180-248.46v-28.16q0-29.38 15.96-54.42 15.96-25.04 42.66-38.5 59.3-29.07 119.65-43.61 60.35-14.54 121.73-14.54t121.73 14.54q60.35 14.54 119.65 43.61 26.7 13.46 42.66 38.5Q780-306 780-276.62v28.16q0 25.3-17.73 43.04-17.73 17.73-43.04 17.73H240.77q-25.31 0-43.04-17.73Q180-223.16 180-248.46Zm60 .77h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.11-16.88-51.7-25.46-105.42-38.58Q534.7-367.69 480-367.69q-54.7 0-108.43 13.11-53.72 13.12-105.42 38.58-12.07 6.54-19.11 16.88-7.04 10.35-7.04 22.5v28.93Zm240-304.62q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm0-80Zm0 384.62Z" />
                     </svg>
@@ -330,7 +368,7 @@ export default function User_exercisePlan() {
                   </a>
                   <a
                     href="/user_panel/user_exercisePlan"
-                    className="w-[90%] border-[2px] border-crimsonRed bg-coal text-mintCream text-[20px] flex justify-between rounded-[10px] max-h-[58px] mx-auto mt-3 py-3 px-3 hover:bg-superRed hover:border-superRed transition-all duration-300"
+                    className="w-[90%] border-[2px] border-crimsonRed bg-crimsonRed text-black text-[20px] flex justify-between rounded-[10px] max-h-[58px] mx-auto mt-3 py-3 px-3 hover:bg-superRed hover:border-superRed transition-all duration-300"
                   >
                     <p>برنامه تمرینی</p>
                     <svg
@@ -340,14 +378,14 @@ export default function User_exercisePlan() {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <g clip-path="url(#clip0_156_1578)">
+                      <g clip-path="url(#clip0_223_1020)">
                         <path
                           d="M24.5347 9.40209L23.0966 7.96395L23.9439 7.08221C24.0323 6.99384 24.0765 6.89445 24.0765 6.78405C24.0765 6.67364 24.0323 6.57437 23.9439 6.48623L20.0138 2.55607C19.9256 2.4677 19.8264 2.42351 19.716 2.42351C19.6055 2.42351 19.5062 2.4677 19.4178 2.55607L18.536 3.40339L17.0635 1.93082L17.9638 0.996054C18.43 0.529874 19.0047 0.302523 19.6881 0.314C20.3714 0.325476 20.9461 0.564304 21.4123 1.03048L25.4695 5.08769C25.9357 5.55387 26.1688 6.12288 26.1688 6.79472C26.1688 7.46656 25.9357 8.03557 25.4695 8.50175L24.5347 9.40209ZM9.00175 24.9695C8.53557 25.4357 7.96656 25.6688 7.29472 25.6688C6.62288 25.6688 6.05387 25.4357 5.58769 24.9695L1.58351 20.965C1.10837 20.4901 0.870809 19.9035 0.870809 19.2053C0.870809 18.507 1.10837 17.9204 1.58351 17.4452L2.43082 16.5979L3.90338 18.0705L3.03472 18.9178C2.94635 19.0062 2.90217 19.1055 2.90217 19.216C2.90217 19.3264 2.94635 19.4256 3.03472 19.5138L6.98623 23.4653C7.07437 23.5536 7.17364 23.5978 7.28405 23.5978C7.39445 23.5978 7.49384 23.5536 7.58221 23.4653L8.42952 22.5966L9.90209 24.0692L9.00175 24.9695ZM22.5378 14.5005L24.3017 12.7366C24.39 12.6482 24.4342 12.5467 24.4342 12.4319C24.4342 12.3171 24.39 12.2156 24.3017 12.1272L14.3728 2.19834C14.2844 2.10997 14.1829 2.06579 14.0681 2.06579C13.9533 2.06579 13.8518 2.10997 13.7634 2.19834L11.9995 3.96218C11.9114 4.05055 11.8673 4.14982 11.8673 4.26C11.8673 4.3704 11.9114 4.46979 11.9995 4.55816L21.9418 14.5005C22.0302 14.5886 22.1296 14.6327 22.24 14.6327C22.3502 14.6327 22.4494 14.5886 22.5378 14.5005ZM13.2022 23.8361L14.966 22.0509C15.0542 21.9628 15.0982 21.8635 15.0982 21.7531C15.0982 21.6427 15.0542 21.5434 14.966 21.4553L5.04473 11.534C4.95659 11.4458 4.85732 11.4018 4.74691 11.4018C4.63651 11.4018 4.53724 11.4458 4.4491 11.534L2.66391 13.2978C2.57554 13.3862 2.53136 13.4878 2.53136 13.6025C2.53136 13.7173 2.57554 13.8189 2.66391 13.9072L12.5928 23.8361C12.6811 23.9245 12.7827 23.9686 12.8975 23.9686C13.0122 23.9686 13.1138 23.9245 13.2022 23.8361ZM12.6114 16.1768L16.6638 12.1458L14.3542 9.83625L10.3232 13.8886L12.6114 16.1768ZM14.6613 25.2873C14.1864 25.7624 13.5985 26 12.8975 26C12.1965 26 11.6085 25.7624 11.1336 25.2873L1.2127 15.3664C0.737566 14.8915 0.5 14.3035 0.5 13.6025C0.5 12.9015 0.737566 12.3136 1.2127 11.8387L2.97654 10.0614C3.45144 9.58652 4.03801 9.34907 4.73624 9.34907C5.43471 9.34907 6.02139 9.58652 6.49629 10.0614L8.8506 12.4161L12.903 8.36403L10.5483 6.03072C10.0734 5.55582 9.83598 4.96558 9.83598 4.26C9.83598 3.55465 10.0734 2.96452 10.5483 2.48962L12.3256 0.712697C12.8005 0.237566 13.3871 0 14.0853 0C14.7835 0 15.3701 0.237566 15.845 0.712697L25.7873 10.655C26.2624 11.1299 26.5 11.7165 26.5 12.4147C26.5 13.1129 26.2624 13.6995 25.7873 14.1744L24.0104 15.9517C23.5355 16.4266 22.9454 16.664 22.24 16.664C21.5344 16.664 20.9442 16.4266 20.4693 15.9517L18.136 13.597L14.0839 17.6494L16.4386 20.0037C16.9135 20.4786 17.1509 21.0653 17.1509 21.7638C17.1509 22.462 16.9135 23.0486 16.4386 23.5235L14.6613 25.2873Z"
-                          fill="#FFF7ED"
+                          fill="black"
                         />
                       </g>
                       <defs>
-                        <clipPath id="clip0_156_1578">
+                        <clipPath id="clip0_223_1020">
                           <rect
                             width="26"
                             height="26"
@@ -435,25 +473,28 @@ export default function User_exercisePlan() {
             planExist ? "flex" : "hidden"
           }`}
         >
-          <div className="flex flex-col justify-start text-center max-h-[400px] overflow-y-auto mx-auto w-full">
-            {exercisePlans.map((exercisePlan) => (
-              <Plan_card
+          <div className="flex flex-col gap-5 justify-center text-center max-h-[550px] overflow-y-auto mx-auto w-full">
+            {/* {exercisePlans.map((exercisePlan) => (
+              <Exe_plan_card
                 key={exercisePlan.name}
                 name={exercisePlan.name}
                 image={exercisePlan.image}
                 description={exercisePlan.description}
                 onClick={() => handleCardClick(exercisePlan)}
               />
+            ))} */}
+            {exercisePlans.map((plan, index) => (
+              <Exe_plan_card key={index} plan={plan} />
             ))}
-        <a href="./mealplan_request" className="flex">
-          <button
-            className={`text-center mx-auto text-irishGreen text-[20px] border-[2px] border-irishGreen rounded-[15px] mt-[30px] py-2 px-5 hover:bg-irishGreen hover:text-black transition-all duration-300 ${
-              planExist ? "flex" : "hidden"
-            }`}
-          >
-            درخواست برنامه تمرینی
-          </button>
-        </a>
+            <a href="./mealplan_request" className="flex">
+              <button
+                className={`text-center mx-auto text-irishGreen text-[20px] border-[2px] border-irishGreen rounded-[15px] mt-[30px] py-2 px-5 hover:bg-irishGreen hover:text-black transition-all duration-300 ${
+                  planExist ? "flex" : "hidden"
+                }`}
+              >
+                درخواست برنامه تمرینی
+              </button>
+            </a>
           </div>
         </div>
       </div>
